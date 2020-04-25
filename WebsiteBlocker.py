5d10adae2facb8b3f11329b0206908ada7a34491
H3I53NB3RG <58789050+H3I53NB3RG@users.noreply.github.com>
2020-04-25 15:22:35 -0700

    Update WebsiteBlocker.py

--- WebsiteBlocker.py
+++ WebsiteBlocker.py
@@ -43,7 +43,7 @@ try:
                     
                 else:
                     with open(hosts_path,'r+') as file:
-                        content=file.readlines()
+                        content = file.readlines()
                         file.seek(0)
                         for line in content:
                             if not any(website in line for website in websiteList):
