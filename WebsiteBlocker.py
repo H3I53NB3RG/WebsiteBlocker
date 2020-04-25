f72ca3a2b8edc6cc1be48fbca91f49036d5ff848
H3I53NB3RG <58789050+H3I53NB3RG@users.noreply.github.com>
2020-04-25 07:16:38 -0700

    Update WebsiteBlocker.py

--- WebsiteBlocker.py
+++ WebsiteBlocker.py
@@ -17,8 +17,8 @@ websitesFile = open("websiteList.txt")
 websiteList = []
 
 try:
-    start = input("Enter The Hour [0-24] At Which u Wanna Start Blocking This Sites : ")
-    end = input("Enter The Hour [0-24]  At Which u Wanna Stop Blocking This Sites : ")
+    start = input("[+] Enter The Hour [0-24] At Which u Wanna Start Blocking This Sites : ")
+    end = input("[+] Enter The Hour [0-24]  At Which u Wanna Stop Blocking This Sites : ")
 
     x = int(start)
     z = int(end)
@@ -49,8 +49,7 @@ try:
                             if not any(website in line for website in websiteList):
                                 file.write(line)
                         file.truncate()
-		
-                print("[+] Running ...")
+
                 time.sleep(5)
             else:
                 print("[-] The Hours Are Not Between 0 and 24")
